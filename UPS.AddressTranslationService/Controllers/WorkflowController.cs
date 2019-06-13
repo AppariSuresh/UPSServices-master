ce66c07418b8c587cdd53222e58f2e9288872d11
Suresh Kumar Appari <suresh.appari@pactera.com>
2019-06-13 21:09:57 +0530

    Add project files.

---
+++ UPS.AddressTranslationService/Controllers/WorkflowController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UPS.AddressTranslationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkflowController : ControllerBase
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult Get()
+        {
+
+            SqlConnection connection = new SqlConnection(@"Data Source=SURESH-APPARI\SQLEXPRESS;Initial Catalog=UpsSf;Integrated Security=True");
+
+            connection.Open();
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("select * from [WR-FLW]", connection);
+
+            DataSet ds = new DataSet();
+
+            sqlDataAdapter.Fill(ds);
+
+            return Ok(ds);
+        }
+        // GET: api/<controller>
+        [HttpPost]
+        public ActionResult Post()
+        {
+
+            SqlConnection connection = new SqlConnection(@"Data Source=SURESH-APPARI\SQLEXPRESS;Initial Catalog=UpsSf;Integrated Security=True");
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+
+            DataSet ds = new DataSet();
+            string sql = null;
+            sql = "insert into [WR-FLW] ([FLE-NA]) values('test1')";
+            try
+            {
+                connection.Open();
+                sqlDataAdapter.InsertCommand = new SqlCommand(sql, connection);
+                sqlDataAdapter.InsertCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+            }
+            sqlDataAdapter.Fill(ds);
+            return this.Get();
+        }
+    }
+}
